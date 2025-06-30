d1872977b0abcd4488fa3493b866c208a86448ec
Ara7736 <ara770702@gmail.com>
2025-06-30 15:14:17 +0400

    Streamlitov

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from screaping import sriping_vega
+import streamlit as st
 
+st.title("vega.am կայքում ավտոմատ որոնման և ստացված արժեքեները արտարժույթով ցուցադրելու համակարգ")
+search_term = st.text_input("Մուտքագրեք ապրանքի անունը")
+lenght_of_list = st.text_input("Մուտքագրեք որոնման երկարությունը")
 
-searching = input()
-lenght_of_list = int(input())
-print(sriping_vega(searching, lenght_of_list))
+if st.button("Որոնել"):
+    if search_term.strip() and lenght_of_list.strip():
+        with st.spinner("Որոնում..."):
+            prices, titles = sriping_vega(search_term, lenght_of_list)
+        st.success("Որոնումը հաջողվեց")
+        for title, price in zip(titles, prices):
+            st.write(f"** {title}** - {price:,} AMD")
+    else:
+        st.warning("Խնդրում եմ մուտքագրեք որոնման նյութը")
