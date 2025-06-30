e8e36aa8f0bcf13e287f1c5666aadeef32a21770
hamhayk <hamhayk88@gmail.com>
2025-06-30 15:36:28 +0400

    added get currency

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from screaping import sriping_vega
 import streamlit as st
+from get_currency import get_rate_by_date
 
 st.title("vega.am կայքում ավտոմատ որոնման և ստացված արժեքեները արտարժույթով ցուցադրելու համակարգ")
 search_term = st.text_input("Մուտքագրեք ապրանքի անունը")
 lenght_of_list = st.text_input("Մուտքագրեք որոնման երկարությունը")
+
+currency = st.selectbox(
+    "Choose currency",
+    ("USD", "EUR", "RUB"),
+index=None,
+placeholder="currency..."
+)
+
+if st.button('Get Rate'):
+    if currency:
+        rate = rate = get_rate_by_date(currency)
+        st.success(f"Exhcange rate for {currency}: {rate}")
+    else:
+        st.warning("Please select a currency first.")
+
 
 if st.button("Որոնել"):
     if search_term.strip() and lenght_of_list.strip():
